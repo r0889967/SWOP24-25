036314815ef8d7b964b9975381de97dbfddc1c49
Jenno <jenno.turtsanszki@gmail.com>
2025-04-03 11:29:31 +0200

    Remove custom cells and relocate Cell class to Domain package

---
+++ Tablr/src/Domain/Cell.java
@@ -0,0 +1,16 @@
+package Domain;
+public class Cell {
+    protected String value;
+
+    public Cell(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
